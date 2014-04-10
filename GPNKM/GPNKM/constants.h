b39e1bd3346b6b2a8d9589e0168a6b4b983e5044
Nuccah <nuccah@teamidk.eu>
2014-04-10 16:24:38 +0200

    Organisation de Afficheur, Serveur, Pilotes dans Main Pipe created between Serveur, Pilotes. Numbers attributed.

--- GPNKM/GPNKM/constants.h
+++ GPNKM/GPNKM/constants.h
@@ -11,6 +11,7 @@
 #include <sys/types.h>
 #include <stdbool.h>
 
+
 /* weather constants */
 #define DRY 1.0
 #define WET 0.9
--- GPNKM/GPNKM/main.c
+++ GPNKM/GPNKM/main.c
@@ -8,59 +8,66 @@ void tester();
 
 int main (int argc, char *argv[])
 {
-	int pfdSrv1[2]; // Creation du pipe entre Serveur et Afficheur du Resultat
+	int pfdSrvAdR[2]; // Creation du pipe entre Serveur et Afficheur du Resultat
 	int pfdAdR[2]; // Creation du pipe entre Serveur et Afficheur du Resultat
-	pipe(pfdSrv1);	 // Creation du pipe entre Serveur et Afficheur du Resultat
+	pipe(pfdSrvAdR);	 // Creation du pipe entre Serveur et Afficheur du Resultat
 	pipe(pfdAdR);	 // Creation du pipe entre Serveur et Afficheur du Resultat
 	int forked;
-	int drivers[] = {1,3,6,7,8,20,11,21,25,19,4,9,44,14,13,22,27,99,26,77,17,10}; // Tableau contenant les #'s des conducteurs
 	forked = fork(); // Premier Fork (Server, Afficheur)
 	if (forked < 0) {
 		perror("Error while attempting Fork (Server/Afficheur de Resultat)");
 		exit(19);
 	}
-	/*Server*/
+	/*Server (Parent)*/
 	else if (forked > 0) {    
-		close(pfdSrv1[0]); // Close unused read end
-		close(pfdAdR[1]); // Close unused write end
-		int pfdSrv2[2]; 	// Creation des pipes entre Serveur et les Pilotes
+		int pfdSrvDrv[2]; 	// Creation des pipes entre Serveur et les Pilotes
 		int pfdDrv[2];	// Creation des pipes entre Serveur et les Pilotes
-		pipe(pfdSrv2);		// Creation des pipes entre Serveur et les Pilotes
+		pipe(pfdSrvDrv);		// Creation des pipes entre Serveur et les Pilotes
 		pipe(pfdAdR);		// Creation des pipes entre Serveur et les Pilotes
 		forked = fork(); // Deuxieme Fork (Server, Pilot)
+		int drivers[] = {1,3,6,7,8,20,11,21,25,19,4,9,44,14,13,22,27,99,26,77,17,10}; // Tableau contenant les #'s des conducteurs
 		if (forked < 0) {
 			perror("Error while attempting Fork (Server/Pilot)");
 			exit(19);
 		}
-		/*Server*/
-		else if (forked > 0) {    
+		/*Server (Parent)*/
+		else if (forked > 0) {
+			int i;
+			close(pfdSrvAdR[0]); // Close unused read end
+			close(pfdAdR[1]); // Close unused write end  
 			const char *weather = randomWeather(); // Weather Selection
 			printf ("Welcome to the worldest famous GPNKM!\n");
-			printf("Weather: %s \n", weather); 
+			printf("Weather: %s \n", weather);
+			for(i=1;i<(sizeof(drivers) / sizeof(int))+1;i++){ // Write in pipe all available numbers
+				write(pfdSrvDrv[1], &drivers[i-1], sizeof(int));
+			} 
 		}
-		/*Pilot*/
+		/*Pilot (Child)*/
 		else{ 
 			int i;
-			close(pfdSrv1[0]);close(pfdSrv1[1]); // Close pipe of Server->Afficheur for Pilots
-			close(pfdAdR[0]);close(pfdAdR[1]); // Close pipe of Server->Afficheur for Pilots
-
+			close(pfdSrvAdR[0]);close(pfdSrvAdR[1]); // Close pipe of Server->Afficheur for Pilots
+			close(pfdAdR[0]);close(pfdAdR[1]); // Close pipe of Afficheur->Server for Pilots
+			close(pfdSrvDrv[1]);close(pfdDrv[1]); // Close unused write/read ends of respective pipes
 			int pid[(sizeof(drivers) / sizeof(int))]; // ????
-			for(i=1;i<(sizeof(drivers) / sizeof(int))+1;i++){
+			for(i=1;i<(sizeof(drivers) / sizeof(int))+1;i++){ // Multifork des 22 pilotes
       			if((pid[i]=fork()) == -1){
          			printf("Error while attempting Fork (Pilot/Pilot)");
           			exit(1);
         		}
       			else if(pid[i]>0){ // DRIVERS //
-          			printf(" \n pid:%3d\n",pid[i]);
-
+					int number;
+          			printf(" \n pid: %3d\n",pid[i]);
+          			read(pfdSrvDrv[0], &number, sizeof(int)); // First come first serve for driver numbers in pipe
+          			const char *team = getTeamName(number); // Return team name according to driver number
+          			printf("Number: %d - Team: %s \n",number, team);
         			break;
         		}
     		} 
 		}
 	}
-	/*Afficheur de Resultat*/
+	/*Afficheur de Resultat (Child)*/
 	else{ 
-		close(pfdSrv1[1]); // Close unused write end
+		close(pfdSrvAdR[1]); // Close unused write end
 		close(pfdAdR[0]); // Close unused read end
 		printf("This is the Display Program \n");
 	}
